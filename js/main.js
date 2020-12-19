4ec03917b7eb867fe1bd7c77203700285f5ade83
sergeiKostenko <sdh1985@mail.ru>
2020-12-19 17:59:49 +0300

    first and second task

--- js/main.js
+++ js/main.js
@@ -1,23 +1,111 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (product, img = 'https://placehold.it/200x150') => {
-    return `<div class="product-item">
-                <img src="${img}">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];//массив товаров
+        //        this.allProducts=[];//массив объектов
+        this._fetchProducts();
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    /*
+    2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
+     */
+    getTotalPrice() {
+        return `Суммарная стоимость всех товаров: ${this.goods.reduce((acc, currentValue) => acc + currentValue.price, 0)}`
+    }
+
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
-};
+    }
+}
 
-renderPage(products);
+/*
+1. Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы 
+понадобятся для работы с этими сущностями.
+ */
+class Basket {
+    constructor() {
+
+    };
+
+    // внешний вид корзины
+    render() {
+
+    };
+
+    // для удаления продукта из корзины
+    removeItem() {
+
+    }
+
+    // для применения промокода
+    getPromo() {
+
+    }
+
+    // подсчет полной стоимости корзины
+    getTotalPrice() {
+
+    }
+}
+
+class BasketItem extends ProductItem {
+    /*
+    При создании экземпляра класса BasketItem по умолчанию будет вызван конструктор базового класса ProductItem, 
+    поэтому в явном виде пока не прописываем
+     */
+
+    // для отрисовки продукта в корзине 
+    render() {
+        super.render();
+    }
+
+    // для увеличения кол-ва конкретного товара в корзине
+    addItemAmount() {
+
+    }
+
+    // для уменьшения кол-ва конкретного товара в корзине
+    subItemAmount() {
+
+    }
+
+}
+
+
+
+let list = new ProductsList();
+list.render();
+console.log(list.getTotalPrice());
 
 
 /*
