89655d2396cf1c102a68c01bd68ba438f5a6cb38
sergeiKostenko <sdh1985@mail.ru>
2021-01-09 15:11:42 +0300

    added catalog components

--- js/main.js
+++ js/main.js
@@ -72,6 +72,54 @@ Vue.component('basket-item', {
 });
 //====================================================================
 
+// Компоненты для построения каталога =================================
+Vue.component('product-list', {
+    props: ['products'],
+    template: `
+    <div>
+        <product-item class="product-item" v-for="product of products" :key="product.id_product"
+        :data-id="product.id_product" :product="product" v-on:add-product="$emit('add-product', product)" v-on:add-favorite="$emit('add-favorite', product)"></product-item>
+    </div>
+    `
+});
+
+
+Vue.component('product-item', {
+    props: ['product'],
+    data() {
+        return {
+            imgCatalog: 'https://placehold.it/200x150'
+        }
+    },
+    template: `
+    <div class="col-md-3 col-xs-6">
+        <div class="product">
+            <div class="product-img">
+                <img :src="imgCatalog" alt="Some img">
+            </div>
+            <div class="product-body">
+                <p class="product-category">Оргтехника</p>
+                <h3 class="product-name"><a href="#">{{product.product_name}}</a></h3>
+                <h4 class="product-price">{{product.price}} &#8381</h4>
+                <div class="product-btns">
+                    <button @click="$emit('add-favorite', product)" class="add-to-wishlist"><i
+                            class="fa fa-heart-o"></i><span class="tooltipp">В
+                            избранное</span></button>
+                    <button class="add-to-compare"><i class="fa fa-exchange"></i><span
+                            class="tooltipp">К сравнению</span></button>
+                    <button class="quick-view"><i class="fa fa-eye"></i><span
+                            class="tooltipp">Просмотр</span></button>
+                </div>
+            </div>
+            <div class="add-to-cart">
+                <button class="add-to-cart-btn" @click="$emit('add-product', product)"><i
+                        class="fa fa-shopping-cart"></i> Купить</button>
+            </div>
+        </div>
+    </div>
+    `
+});
+//====================================================================
 
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
@@ -86,7 +134,6 @@ const app = new Vue({
         favorites: [],
         imgCatalog: 'https://placehold.it/200x150',
         // imgBasket: 'https://placehold.it/100x100',
-
         // userSearch: '',
         userEmail: '',
         placeholder: 'Введите Вашу почту',
@@ -101,9 +148,6 @@ const app = new Vue({
                 .catch(error => {
                     console.log(error);
                 })
-        },
-        sayHello() {
-            console.log('hello')
         },
         addProduct(product) {
             this.getJson(`${API}/addToBasket.json`)
