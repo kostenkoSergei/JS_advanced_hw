545c4cce0904847ec4d9c41d03e8a41d5d8c7dfb
sergeiKostenko <sdh1985@mail.ru>
2021-01-21 19:16:36 +0300

    final version js2 course shop

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'public'),
-    mode:'development',
+    mode: 'development',
     entry: './js/main.js',
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
     watch: true,
-    plugins:[
+    plugins: [
         new HTMLWebpackPlugin({
             template: './index.html'
         }),
@@ -20,18 +20,20 @@ module.exports = {
             filename: '[name].[contenthash].css'
         }),
         new CleanWebpackPlugin()
-        ],
-    module:{
-        rules:[
+    ],
+    module: {
+        rules: [
             {
                 test: /\.css$/,
                 use: [
-                    {loader: MiniCssExtractPlugin.loader,
-                    options:{
-                    }},
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                        }
+                    },
                     'css-loader'
                 ]
-            }
+            },
         ]
     }
 }
